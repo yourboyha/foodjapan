4f0bde70b40c38e05b5588e1be97df74a63fc65b
yourboyha <w.wimonput@gmail.com>
2023-08-08 19:49:59 +0700

    page1

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,5 +1,33 @@
-const Menu = () => {
-  return <h1>menu</h1>;
-};
+// const Menu = () => {
+//   return <h1>menu</h1>;
+// };
+
+import { Container, Table } from "react-bootstrap";
+
+
+function Menu() {
+  const data =[
+    
+  ]
+
+  return (
+    <Container className='p-3 my-3' >
+      <h1>เมนูอาหารญี่ปุ่น</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>ชื่อเมนู</th>
+            <th>ราคา (บาท)</th>
+          </tr>
+
+        </thead>
+      </Table>
+
+
+    </Container >
+  )
+}
+
 
 export default Menu;
